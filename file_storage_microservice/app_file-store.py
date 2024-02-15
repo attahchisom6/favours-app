6ac77ff55e56c0a77bd53382463c02d9143a9939
attahchisom6 <attahchisom@gmail.com>
2024-02-15 08:17:41 +0100

    ADD

--- file_storage_microservice/app_file-store.py
+++ file_storage_microservice/app_file-store.py
@@ -31,7 +31,31 @@ def create_basemodel_instance():
     basemodel_instance.save()
     return jsonify({"message": "instance created successfully"}), 201
   else:
-    return f"No data parsed: {data}"
+    return jsonify({"message": f"No data parsed: here data is {data}"}),201
+  
+
+@app.route("/update/<cls>/<id>", methods=["POST"], strict_slashes=False)
+def update_basemodel_instance(cls, id):
+  """
+  update the basemodel instance
+  """
+  all_obj = storage.get(cls, id)
+  data = request.json
+  if all_obj:
+    for key, value in data:
+      if key in all_obj:
+        all_obj[key] = value
+    return jsonify({"message": "base_model instances updated succesfully"}), 201
+  else:
+    return jsonify({"message": "Nodata  found for update"}), 404
+
+
+@app.route("/delete/<cls>/<id>", methods=["DELETE"], strict_slashes=False)
+def delete_basemodel_instance(cls, id):
+  """
+  delea=te a basemodel instance
+  """
+  all_obj = storage.get(cls, id)
 
 
 if __name__ == "__main__":
