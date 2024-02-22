2e2ef3d1f5de8e5880b4f1e8e642880e524708f2
attahchisom6 <attahchisom6@gmail.com>
2024-02-22 09:56:20 +0100

    afs

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@ from os import getenv
 storage_t = getenv("FAVOURS_ENV")
 
 if storage_t == "db":
-  from models.engine.db_storage import DbStorage
+  from models.engine.db_storage import DBStorage
   storage = DBStorage()
 else:
   from models.engine.file_storage import FileStorage
