016c2cf770e6e10fdd083f3c7151cbfd69ba71d3
attahchisom6 <attahchisom6@gmail.com>
2024-02-25 17:22:23 +0100

    aff

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,7 +11,6 @@ from os import getenv
 
 
 classes = {
-    "BaseModel": BaseModel,
     "User": User
   }
 
@@ -75,12 +74,7 @@ class DBStorage:
 
     for cl in classes:
       if cls is None or cls is classes[cl] or cls == cl:
-        table = None
-        try:
-          table = classes[cl].__table__
-        except AttributeError:
-          print(f"class {classes[cl]} does not provide a table in the database, either its related to a file storage or something else")
-          return
+        table = classes[cl].__table__
         objs = self.__session.query(table).all()
         parse_objs(objs)
     return obj_dict
