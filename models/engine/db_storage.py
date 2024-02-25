8943d856878780f728bb446183c5665a2ec4e67d
attahchisom6 <attahchisom6@gmail.com>
2024-02-25 17:07:50 +0100

    add

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,12 +75,12 @@ class DBStorage:
 
     for cl in classes:
       if cls is None or cls is classes[cl] or cls == cl:
-        table = None
+        """table = None
         try:
           table = classes[cl].__table__
         except AttributeError:
-          print(f"class {classes[cl]} does not provide a table in the database, either its related to a file storage or something else")
-        objs = self.__session.query(table).all()
+          print(f"class {classes[cl]} does not provide a table in the database, either its related to a file storage or something else")"""
+        objs = self.__session.query(classes[cl]).all()
         parse_objs(objs)
     return obj_dict
 
