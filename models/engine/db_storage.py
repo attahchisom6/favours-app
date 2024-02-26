f603cb18b8ea8d460fb156bae7e15f33b312b675
attahchisom6 <attahchisom6@gmail.com>
2024-02-26 18:59:30 +0100

    add

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -94,7 +94,7 @@ class DBStorage:
     Base.metadata.create_all(self.__engine)
     session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
     Session = scoped_session(session_factory)
-    self.__session = Session()
+    self.__session = Session
 
   def close(self):
     """
