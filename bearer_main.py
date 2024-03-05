a527680f28b5b676a0c263c255038fb50d7a1468
attahchisom6 <attahchisom6@gmail.com>
2024-03-05 08:48:07 +0100

    add

--- bearer_main.py
+++ bearer_main.py
@@ -18,6 +18,8 @@ print(b.extract_user_from_credentials("pas", None))
 print(b.extract_user_from_credentials(u.email, "i5"))
 print(b.extract_user_from_credentials(u.email, "127oke"))
 print(u.is_valid_password("127oke"))
+print(u.email)
+print(u.password)
 # assert u.password == bcrypt.hashpw("127oke".encode("utf-8"), bcrypt.gensalt())
 
 jwt_encoded = jwt.encode(kwargs, key=b.load_from_env_or_file(), algorithm="HS384")
