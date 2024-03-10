660a6d5b4a7bda60cb160fd6d3ba719777914416
attahchisom6 <attahchisom6@gmail.com>
2024-03-10 10:20:21 +0100

    asd

--- Authentication_microservice/api/v1/views/users.py
+++ Authentication_microservice/api/v1/views/users.py
@@ -105,7 +105,7 @@ def create_db_user():
   res = None
   try:
     res = requests.post(f"{db_url}/create/User", json=data)
-    # print(f"({res.text()} {res.status_code})")
+    print(f"({res.text()} {res.status_code})")
   except Exception as e:
     return jsonify({"message": f"Failecd to create a user instance for the database: {str(e)}"}), 500
 
