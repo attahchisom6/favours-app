b133a0f99672c04efe3c6439e58a8f3aff243f2c
attahchisom6 <attahchisom6@gmail.com>
2024-03-10 18:20:33 +0100

    aff

--- models/user.py
+++ models/user.py
@@ -113,9 +113,10 @@ class User(BaseModel, Base):
     existing_users = User.search({"email": self.email})
     if existing_users:
       print(f"existing_users: {existing_users}")
-      for user in existing_users:
+      """for user in existing_users:
         if user.id == self.id:
           print(f"self: {self}")
           print(f"user: {user}")
-          return
+          return"""
+      return
     super().save()
